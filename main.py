5cabae07687d061178475921a55911c8bd00a2fe
burkkyy <calebburke91@gmail.com>
2025-10-27 12:12:53 -0700

    Set up python venv with uv and added manim

---
+++ main.py
@@ -0,0 +1,14 @@
+from manim import *
+
+
+class S(Scene):
+    def __init__(self):
+        pass
+
+
+def main():
+    print("Hello from csci429-project!")
+
+
+if __name__ == "__main__":
+    main()
